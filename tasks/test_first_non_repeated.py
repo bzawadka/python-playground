15cd18b9f561866a10a5b751eb610b837de9621f
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2025-06-19 22:06:41 +0200

    task: cleanup

--- tasks/test_first_non_repeated.py
+++ tasks/test_first_non_repeated.py
@@ -1,9 +1,31 @@
-from first_non_repeated import *
+# return first non-repeated character in the string
+class Solution:
+    input_string_empty, all_characters_repreated = '', ''
+
+    def first_non_repeated(self, input_str: str) -> str:
+        if not input_str: return self.input_string_empty
+
+        char_counts = dict()
+        for c in input_str:
+            char_counts[c] = char_counts[c] + 1 if c in char_counts else 1 
+
+        if all(char_counts[c] > 1 for c in char_counts):
+            return self.all_characters_repreated
+
+        first_non_repeated = ''
+        for c in input_str:
+            if char_counts[c] == 1:
+                first_non_repeated = c
+                break
+
+        return first_non_repeated
+
 
 def test_first_non_repeated():
     s = Solution()
     assert s.first_non_repeated("") == ""
     assert s.first_non_repeated("!") == "!"
+    assert s.first_non_repeated("alalp") == "p"
     assert s.first_non_repeated("total") == "o"
     assert s.first_non_repeated("teeter") == "r"
     assert s.first_non_repeated("Bartek") == "B"
