055900447feed6c1d822423b1cec761ff3bc4bdc
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2025-07-09 22:33:47 +0200

    may task: cleanup

--- may-interview/test_solution.py
+++ may-interview/test_solution.py
@@ -21,7 +21,7 @@ def test_init_solution():
 def test_move_left_adds_numbers():
     s = Solution()
     s.init_board([(0, 0), (0, 2), (1, 1), (1, 3), (2, 0), (2, 2), (3, 1), (3, 3)])
-    state = s.make_move(Direction.LEFT)
+    s.make_move(Direction.LEFT)
     assert s.board == [
         [4, 0, 0, 0],
         [4, 0, 0, 0],
@@ -146,15 +146,15 @@ def test_requirement_3_merge_right():
 def test_requirement_4_merge_up():
     s = Solution()
     s.board = [
-        [0, 8  , 2  , 2  ],
-        [4  , 2  , 0, 2  ],
+        [0, 8, 2, 2],
+        [4, 2, 0, 2],
         [0, 0, 0, 0],
-        [0, 0, 0, 2  ]]
+        [0, 0, 0, 2]]
 
     s.make_move(Direction.UP)
     assert s.board == [
-        [4  , 8  , 2  , 4  ],
-        [0, 2  , 0, 2  ],
+        [4, 8, 2, 4],
+        [0, 2, 0, 2],
         [0, 0, 0, 0],
         [0, 0, 0, 0]]
 
@@ -162,31 +162,31 @@ def test_requirement_4_merge_up():
 def test_requirement_4_merge_down():
     s = Solution()
     s.board = [
-        [0, 8  , 2  , 2  ],
-        [4  , 2  , 0, 2  ],
+        [0, 8, 2, 2],
+        [4, 2, 0, 2],
         [0, 0, 0, 0],
-        [0, 0, 0, 2  ]]
+        [0, 0, 0, 2]]
 
     s.make_move(Direction.DOWN)
     assert s.board == [
         [0, 0, 0, 0],
         [0, 0, 0, 0],
-        [0, 8  , 0, 2],
-        [4  , 2  , 2  , 4]]
+        [0, 8, 0, 2],
+        [4, 2, 2, 4]]
 
 
 def test_requirement_5_generate_new_item():
     s = Solution()
     s.board = [
-        [0, 8  , 2  , 2  ],
-        [4  , 2  , 0, 2  ],
+        [0, 8, 2, 2],
+        [4, 2, 0, 2],
         [0, 0, 0, 0],
-        [0, 0, 0, 2  ]]
+        [0, 0, 0, 2]]
 
     s.make_move(Direction.UP, generate_new_item=True)
 
     # after the move, first row is always populated
-    assert s.board[0] == [4  , 8  , 2  , 4  ]
+    assert s.board[0] == [4, 8, 2, 4]
 
     # remaining rows should contain two original cells with number 2, and one new - generated
     remainder = s.board[1:]
